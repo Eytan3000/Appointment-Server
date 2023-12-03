a7e9ca07c177bb0e0762907063a4c7efd18be21c
Eytan3000 <eytankrief@gmail.com>
2023-12-03 14:51:28 +0200

    for first deployment

--- database.js
+++ database.js
@@ -2,12 +2,23 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = mysql
+// const pool = mysql
+//   .createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//   })
+//   .promise();
+
+
+  const pool = mysql
   .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   })
   .promise();
 
